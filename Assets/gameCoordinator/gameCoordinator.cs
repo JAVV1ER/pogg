f8c3bbc11f30cb90f4bc295b22b10653d2700f25
JVR <danila.iva2102@gmail.com>
2023-08-25 00:17:37 +0300

    test pravki

--- Assets/gameCoordinator/gameCoordinator.cs
+++ Assets/gameCoordinator/gameCoordinator.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using System;
 public class gameCoordinator : MonoBehaviour
 {
+    private TMP_Text botScore;
+    private TMP_Text playerScore;
+    public GameObject bot;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        botScore = bot.GetComponent<TMP_Text>();
+        playerScore = player.GetComponent<TMP_Text>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((botScore.text == "7") || (playerScore.text == "7"))
+        {
+            Debug.Log("WIN");
+        }
     }
 }
